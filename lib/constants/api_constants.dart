76fed2b1bfdfee821da98bf5fb6498084433fbfa
ruzafelipe <ruzafelipe@gmail.com>
2023-10-17 22:55:59 -0300

    Ocultar a chave da API

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   static const apiUrl = 'api.openweathermap.org';
-  static const apiKey = '6145e350e701e65af39637d93306eccc'; //cada um deve ter a SUA
+  static const apiKey = 'XXXXXXXXXXXX'; //cada um deve ter a SUA
   static const geoPath = '/geo/1.0/direct';
   static const weatherPath = '/data/2.5/weather';
   static const units = 'metric';
